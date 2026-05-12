2245ac42ec74f459f4ae86223c153deccd9820a9
yathisha-coder <yathisai85@gmail.com>
2026-05-12 13:47:17 -0400

    In house class Added getters/ setters and override.

--- src/main/java/org/example/House.java
+++ src/main/java/org/example/House.java
@@ -13,4 +13,55 @@ public class House extends Asset{
         this.squareFoot = squareFoot;
         this.lotSize = lotSize;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+    public double getValue(){
+        double pricePerSquareFoot = 0;
+
+        switch (condition){
+            case 1:
+                 pricePerSquareFoot =180.00;
+                break;
+            case 2:
+                pricePerSquareFoot = 130.00;
+                break;
+            case 3:
+                pricePerSquareFoot = 90.00;
+                break;
+            case 4:
+                pricePerSquareFoot = 80.00;
+                break;
+        }
+        return (squareFoot * pricePerSquareFoot) +(lotSize);
+    }
 }
